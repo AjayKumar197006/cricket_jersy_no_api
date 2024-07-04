902f72efebf34e3e55761f48f227d02e4eaa18a9
Sindhura <ajaykumar197006@outlook.com>
2024-07-05 05:01:07 +0530

    api

---
+++ db.js
@@ -0,0 +1,113 @@
+const sql=require("mysql2")
+
+const con=sql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'uday51@$man',
+        database:'cricket'
+    }
+)
+
+function getData()
+{
+    return new Promise(function(success,reject)
+{
+    con.query(`SELECT * from jersynumbers `,function(err,rows,col)
+    {
+        if(err)
+            {
+                reject(error)
+            }
+            else
+            {
+                success(rows)
+            }
+    })
+})
+}
+
+function addData(nu,na,co)
+{
+    return new Promise(function(success,reject)
+    {
+    
+    con.query(`INSERT INTO jersynumbers(number,name,country) VALUES(?,?,?)`,[nu,na,co]),function(err,res)
+    {
+        if(err)
+            {
+                reject(err)
+
+            }
+        else
+        {
+            success(res)
+        }
+    }
+  })
+}
+
+function updateData(nu,na,co,n)
+{
+    return new Promise(function(success,reject)
+{
+    con.query(`UPDATE jersynumbers SET number=?,name=?,country=? where number=?`,[nu,na,co,n],function(err,res)
+{
+    if(err)
+        {
+            reject(error)
+        }
+    else
+    {
+        success(res)
+    }
+})
+})
+}
+
+function getPlayer(nu)
+{
+    return new Promise(function(success,reject)
+{
+    con.query(`SELECT * from jersynumbers WHERE number=?`,[nu],function(err,res)
+{
+    if(err)
+        {
+            reject(err)
+        }
+    else
+    {
+        console.log(nu)
+        success(res)
+        
+    }
+})
+
+})
+    
+}
+
+function deleteData(nu)
+{
+    return new Promise(function(success,reject)
+    {
+        con.query(`DELETE FROM jersynumbers where number=?`,[nu],function(err,res)
+        {
+            if(err)
+                {
+                    reject(err)
+                }
+            else
+            {
+                console.log(res)
+                success(res)
+            }
+        })
+        
+
+    })
+
+}
+
+
+module.exports={addData,getData,updateData,getPlayer,deleteData}
---
+++ index.js
@@ -0,0 +1,88 @@
+const express=require("express")
+
+const app=express()
+const db=require("./db")
+app.use(express.json());
+
+app.listen(8000,()=>
+{
+    console.log("server started")
+    
+})
+app.get("/player",(req,res)=>
+{
+    db.getData()
+    .then((data)=>
+        {
+            res.send(data)
+            console.log(data)
+        })
+    .catch((error)=>
+    {
+        console.log(error)
+        res.send(error)
+    })
+})
+app.get("/players/:number",(req,res)=>
+{
+    console.log(req.body.number)
+    const id=req.params.number
+    db.getPlayer(id)
+    .then((data)=>
+    {
+        res.send(data)
+    })
+    .catch((error)=>
+  {
+    console.log(error)
+    res.send(error)
+})
+})
+
+app.post("/add",(req,res)=>
+{
+    db.addData(req.body.number,req.body.name,req.body.country)
+    .then((data)=>
+    {
+        res.send(data)
+    })
+    .catch((error)=>
+    {
+        console.log(error)
+        res.send(error)
+    })
+})
+app.put("/update/:number",(req,res)=>
+{
+    const id=req.params.number
+    db.updateData(req.body.number,req.body.name,req.body.country,id)
+    .then(()=>
+    {
+        res.send(req.body)
+    })
+    .catch(()=>
+    {
+        console.log(error)
+        res.send(error)
+
+    })
+})
+
+app.delete("/delete/:number",(req,res)=>
+{
+    const id=req.params.number
+    console.log(id)
+    db.deleteData(id)
+    .then(()=>
+    {
+        res.send(req.body)
+    })
+    .catch(()=>
+    {
+        console.log(error)
+        res.send(error)
+    })
+})
+
+
+
